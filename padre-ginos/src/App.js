6e4a8a5a033b9a4cddd5a22e2046ba3069363633
Victor Goico <vgoico000@citymail.cuny.edu>
2025-06-03 23:00:49 -0400

    Init, prettier

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,36 +1,32 @@
 const Pizza = (props) => {
-    return React.createElement("div", {} , [
-        React.createElement("h1", {}, props.title),
-        React.createElement("p", {}, props.description)
-    ])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.title),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Padre Gino's"),
-            React.createElement(Pizza, {
-                title: "PEPPERONI PIZZA",
-                description: "ABC 123 CHEESE PIZZA"
-            }),
-            React.createElement(Pizza, {
-                title: "GRANDMA PIZZA",
-                description: "ABC 123 GRANDMA PIZZA"
-            }),
-            React.createElement(Pizza, {
-                title: "Lowercase PIZZA",
-                description: "ABC 123 Lowercase PIZZA"
-            }),
-            React.createElement(Pizza, {
-                title: "Pineapple Pizza",
-                description: "ABC 123 Pineapple Pizza"
-            })
-        ]
-    )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      title: "PEPPERONI PIZZA",
+      description: "ABC 123 CHEESE PIZZA",
+    }),
+    React.createElement(Pizza, {
+      title: "GRANDMA PIZZA",
+      description: "ABC 123 GRANDMA PIZZA",
+    }),
+    React.createElement(Pizza, {
+      title: "Lowercase PIZZA",
+      description: "ABC 123 Lowercase PIZZA",
+    }),
+    React.createElement(Pizza, {
+      title: "Pineapple Pizza",
+      description: "ABC 123 Pineapple Pizza",
+    }),
+  ]);
+};
 
-const container = document.getElementById("root")
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App))
+root.render(React.createElement(App));
