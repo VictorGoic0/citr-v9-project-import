3888aeca23d5d4d95d1d479c4a89d90d536fe576
Victor Goico <vgoico000@citymail.cuny.edu>
2025-06-05 18:11:43 -0400

    Addign pizza class and form component

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,22 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order"
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's</h1>
-      <Pizza title="PEPPERONI PIZZA" description="ABC 123 CHEESE PIZZA" image={"/public/pizzas/pepperoni.webp"}></Pizza>
-      <Pizza title="GRANDMA PIZZA" description="ABC 123 GRANDMA PIZZA" image={"/public/pizzas/hawaiian.webp"}></Pizza>
-      <Pizza
-        title="Lowercase PIZZA"
-        description="ABC 123 Lowercase PIZZA"
-        image={"/public/pizzas/big_meat.webp"}
-      ></Pizza>
-      <Pizza
-        title="Pineapple Pizza"
-        description="ABC 123 Pineapple Pizza"
-        image={"/public/pizzas/pepperoni.webp"}
-      ></Pizza>
+      <Order></Order>
     </div>
   );
 };
--- padre-ginos/src/Pizza.jsx
+++ padre-ginos/src/Pizza.jsx
@@ -1,9 +1,9 @@
 const Pizza = (props) => {
     return (
-        <div>
-        <h1>{props.title}</h1>
-        <p>{props.description}</p>
-        <img src={props.image} alt={props.title} />
+        <div className="pizza">
+            <h1>{props.title}</h1>
+            <p>{props.description}</p>
+            <img src={props.image} alt={props.title} />
     </div>
     )
 };
