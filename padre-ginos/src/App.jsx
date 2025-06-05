a8354d1f157630c3563a5c1c0f24535e9840f78b
Victor Goico <vgoico000@citymail.cuny.edu>
2025-06-05 14:56:06 -0400

    Adding images

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -5,15 +5,17 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's</h1>
-      <Pizza title="PEPPERONI PIZZA" description="ABC 123 CHEESE PIZZA"></Pizza>
-      <Pizza title="GRANDMA PIZZA" description="ABC 123 GRANDMA PIZZA"></Pizza>
+      <Pizza title="PEPPERONI PIZZA" description="ABC 123 CHEESE PIZZA" image={"/public/pizzas/pepperoni.webp"}></Pizza>
+      <Pizza title="GRANDMA PIZZA" description="ABC 123 GRANDMA PIZZA" image={"/public/pizzas/hawaiian.webp"}></Pizza>
       <Pizza
         title="Lowercase PIZZA"
         description="ABC 123 Lowercase PIZZA"
+        image={"/public/pizzas/big_meat.webp"}
       ></Pizza>
       <Pizza
         title="Pineapple Pizza"
         description="ABC 123 Pineapple Pizza"
+        image={"/public/pizzas/pepperoni.webp"}
       ></Pizza>
     </div>
   );
--- padre-ginos/src/Pizza.jsx
+++ padre-ginos/src/Pizza.jsx
@@ -3,6 +3,7 @@ const Pizza = (props) => {
         <div>
         <h1>{props.title}</h1>
         <p>{props.description}</p>
+        <img src={props.image} alt={props.title} />
     </div>
     )
 };
