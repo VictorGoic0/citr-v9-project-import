dc6b1d8fe26305ba2749c5cca63e06fd26b15ba0
Victor Goico <vgoico000@citymail.cuny.edu>
2025-07-02 13:51:27 -0400

    Done with past order modal

--- padre-ginos/src/routes/past.lazy.jsx
+++ padre-ginos/src/routes/past.lazy.jsx
@@ -2,6 +2,13 @@ import { createLazyFileRoute } from '@tanstack/react-router'
 import { useState } from "react"
 import { useQuery } from "@tanstack/react-query";
 import getPastOrders from '../api/getPastOrders'
+import getPastOrder from '../api/getPastOrder';
+import Modal from '../Modal'
+
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
 export const Route = createLazyFileRoute('/past') ({
   component: PastOrdersRoute,
@@ -13,6 +20,15 @@ function PastOrdersRoute() {
     queryKey: ["past-orders", page],
     queryFn: () => getPastOrders(page),
     staleTime: 30000
+  })
+
+  const [focusedOrder, setFocusedOrder] = useState();
+
+  const { isLoading: isLoadingPastOrder, data: pastOrderData } = useQuery({
+    queryKey: ["past-order", focusedOrder],
+    queryFn: () => getPastOrder(focusedOrder),
+    enabled: !!focusedOrder,
+    staleTime: 24 * 60 * 60 * 1000
   })
 
   if (isLoading) {
@@ -34,7 +50,7 @@ function PastOrdersRoute() {
         <tbody>
           {data.map((order) => (
             <tr key={order.order_id}>
-              <td>{order.order_id}</td>
+              <button onClick={() => setFocusedOrder(order.order_id)}>{order.order_id}</button>
               <td>{order.date}</td>
               <td>{order.time}</td>
             </tr>
@@ -50,6 +66,43 @@ function PastOrdersRoute() {
           Next
         </button>
       </div>
+
+      {focusedOrder ? (
+        <Modal>
+          <h2>Order # {focusedOrder}</h2>
+          {!isLoadingPastOrder ? (
+            <table>
+              <thead>
+                <tr>
+                  <td>Image</td>
+                  <td>Name</td>
+                  <td>Size</td>
+                  <td>Quantity</td>
+                  <td>Price</td>
+                  <td>Total</td>
+                </tr>
+              </thead>
+              <tbody>
+                {pastOrderData.orderItems.map((pizza) => (
+                  <tr key={`${pizza.pizzaTypeId}_${pizza.size}`}>
+                    <td>
+                      <img src={pizza.image} alt={pizza.name} />
+                    </td>
+                    <td>{pizza.name}</td>
+                    <td>{pizza.size}</td>
+                    <td>{pizza.quantity}</td>
+                    <td>{intl.format(pizza.price)}</td>
+                    <td>{intl.format(pizza.total)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>Loading …</p>
+          )}
+          <button onClick={() => setFocusedOrder()}>Close</button>
+        </Modal>
+      ) : null}
     </div>
   )
 }
